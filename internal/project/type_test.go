7b814475a61f28e4a23ec32885fd3be5c25e3e2f
leonsteinhaeuser <lsteinha@redhat.com>
2025-03-02 02:06:56 +0100

    chore: add tests for EnvStageProperty and EnvStageClusterProperty

--- internal/project/type_test.go
+++ internal/project/type_test.go
@@ -87,3 +87,200 @@ func TestProjectConfig_AddonGroups(t *testing.T) {
 		})
 	}
 }
+
+func TestProjectConfig_EnvStageProperty(t *testing.T) {
+	type fields struct {
+		BasePath         string
+		TemplateBasePath string
+		Addons           map[string]Addon
+		ParsedAddons     map[string]template.TemplateManifest
+		Environments     map[string]Environment
+	}
+	type args struct {
+		environment string
+		stage       string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   map[string]string
+	}{
+		{
+			name: "should merge the properties of the environment and stage",
+			fields: fields{
+				Environments: map[string]Environment{
+					"env1": {
+						Properties: map[string]string{
+							"env1": "env1",
+						},
+						Stages: map[string]Stage{
+							"stage1": {
+								Properties: map[string]string{
+									"stage1": "stage1",
+								},
+							},
+						},
+					},
+				},
+			},
+			args: args{
+				environment: "env1",
+				stage:       "stage1",
+			},
+			want: map[string]string{
+				"env1":   "env1",
+				"stage1": "stage1",
+			},
+		},
+		{
+			name: "should return the properties of the stage as the stage has precedence",
+			fields: fields{
+				Environments: map[string]Environment{
+					"env1": {
+						Properties: map[string]string{
+							"env1": "env1",
+						},
+					},
+				},
+			},
+			args: args{
+				environment: "env1",
+				stage:       "stage1",
+			},
+			want: map[string]string{
+				"env1": "env1",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pc := &ProjectConfig{
+				BasePath:         tt.fields.BasePath,
+				TemplateBasePath: tt.fields.TemplateBasePath,
+				Addons:           tt.fields.Addons,
+				ParsedAddons:     tt.fields.ParsedAddons,
+				Environments:     tt.fields.Environments,
+			}
+
+			got := pc.EnvStageProperty(tt.args.environment, tt.args.stage)
+			if diff := cmp.Diff(got, tt.want); diff != "" {
+				t.Errorf("ProjectConfig.EnvStageProperty() mismatch (-got +want):\n%s", diff)
+				return
+			}
+		})
+	}
+}
+
+func TestProjectConfig_EnvStageClusterProperty(t *testing.T) {
+	type fields struct {
+		BasePath         string
+		TemplateBasePath string
+		Addons           map[string]Addon
+		ParsedAddons     map[string]template.TemplateManifest
+		Environments     map[string]Environment
+	}
+	type args struct {
+		environment string
+		stage       string
+		cluster     string
+	}
+	tests := []struct {
+		name   string
+		fields fields
+		args   args
+		want   map[string]string
+	}{
+		{
+			name: "should merge the properties of the environment, stage and cluster",
+			fields: fields{
+				Environments: map[string]Environment{
+					"env1": {
+						Properties: map[string]string{
+							"env1": "env1",
+						},
+						Stages: map[string]Stage{
+							"stage1": {
+								Properties: map[string]string{
+									"stage1": "stage1",
+								},
+								Clusters: map[string]Cluster{
+									"cluster1": {
+										Properties: map[string]string{
+											"cluster1": "cluster1",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			args: args{
+				environment: "env1",
+				stage:       "stage1",
+				cluster:     "cluster1",
+			},
+			want: map[string]string{
+				"env1":     "env1",
+				"stage1":   "stage1",
+				"cluster1": "cluster1",
+			},
+		},
+		{
+			name: "should use the properties of the cluster as the cluster has precedence",
+			fields: fields{
+				Environments: map[string]Environment{
+					"env1": {
+						Properties: map[string]string{
+							"env1": "env1",
+						},
+						Stages: map[string]Stage{
+							"stage1": {
+								Properties: map[string]string{
+									"stage1": "stage1",
+								},
+								Clusters: map[string]Cluster{
+									"cluster1": {
+										Properties: map[string]string{
+											"env1":     "b",
+											"stage1":   "a",
+											"cluster1": "c",
+										},
+									},
+								},
+							},
+						},
+					},
+				},
+			},
+			args: args{
+				environment: "env1",
+				stage:       "stage1",
+				cluster:     "cluster1",
+			},
+			want: map[string]string{
+				"env1":     "b",
+				"stage1":   "a",
+				"cluster1": "c",
+			},
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			pc := &ProjectConfig{
+				BasePath:         tt.fields.BasePath,
+				TemplateBasePath: tt.fields.TemplateBasePath,
+				Addons:           tt.fields.Addons,
+				ParsedAddons:     tt.fields.ParsedAddons,
+				Environments:     tt.fields.Environments,
+			}
+			got := pc.EnvStageClusterProperty(tt.args.environment, tt.args.stage, tt.args.cluster)
+			diff := cmp.Diff(got, tt.want)
+			if diff != "" {
+				t.Errorf("ProjectConfig.EnvStageClusterProperty() mismatch (-got +want):\n%s", diff)
+				return
+			}
+		})
+	}
+}
