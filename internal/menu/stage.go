cfed70e0c44e18d211627f4c6fb283f06a6d2862
leonsteinhaeuser <lsteinha@redhat.com>
2025-03-06 14:10:42 +0100

    feat: implement stage creation and update properties menu

--- internal/menu/stage.go
+++ internal/menu/stage.go
@@ -2,11 +2,14 @@ package menu
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"io"
 
 	"github.com/leonsteinhaeuser/openshift-gitops-cli/internal/cli"
 	"github.com/leonsteinhaeuser/openshift-gitops-cli/internal/project"
+	"github.com/leonsteinhaeuser/openshift-gitops-cli/internal/utils"
+	"github.com/manifoldco/promptui"
 )
 
 type stageMenu struct {
@@ -16,7 +19,7 @@ type stageMenu struct {
 }
 
 func (s *stageMenu) menuCreateStage(env string) (*project.Stage, error) {
-	stage, err := cli.StringQuestion(s.writer, s.reader, "Stage Name", "", func(str string) error {
+	stageName, err := cli.StringQuestion(s.writer, s.reader, "Stage Name", "", func(str string) error {
 		if str == "" {
 			return fmt.Errorf("stage name cannot be empty")
 		}
@@ -29,22 +32,69 @@ func (s *stageMenu) menuCreateStage(env string) (*project.Stage, error) {
 		return nil, err
 	}
 
-	// TODO: ask for properties
-
-	return &project.Stage{
-		Name:       stage,
+	stage := &project.Stage{
+		Name:       stageName,
 		Properties: map[string]string{},
 		Actions:    project.Actions{},
 		Clusters:   map[string]*project.Cluster{},
-	}, nil
+	}
+
+	properties, err := s.menuProperties(stage)
+	if err != nil {
+		return nil, err
+	}
+	stage.Properties = properties
+
+	return stage, nil
 }
 
-func (s *stageMenu) menuUpdateStage(env, stage string) (*project.Stage, error) {
-	// TODO: menu is missing to update properties
-	return s.config.Environments[env].Stages[stage], nil
+func (s *stageMenu) menuUpdateStage(envName, stageName string) (*project.Stage, error) {
+	stage := s.config.Environments[envName].Stages[stageName]
+	properties, err := s.menuProperties(stage)
+	if err != nil {
+		return nil, err
+	}
+	stage.Properties = properties
+	return stage, nil
 }
 
 func (s *stageMenu) menuDeleteStage(env, stage string) error {
 	// TODO: menu is missing to delete the stage (cascade delete)
-	return nil
+	return errors.New("not implemented")
+}
+
+func (s *stageMenu) menuProperties(stage *project.Stage) (map[string]string, error) {
+	stageProperties := map[string]string{}
+	for {
+		properties := utils.MergeMaps(stage.Properties, stageProperties)
+
+		prompt := promptui.SelectWithAdd{
+			Label:    "Properties",
+			Items:    append(utils.MapKeysToList(properties), "Done"),
+			AddLabel: "Create Property",
+		}
+		_, result, err := prompt.Run()
+		if err != nil {
+			return nil, err
+		}
+		if result == "" {
+			return nil, fmt.Errorf("property key cannot be empty")
+		}
+		if result == "Done" {
+			// user is done
+			break
+		}
+
+		val, err := cli.StringQuestion(s.writer, s.reader, "Property Value", properties[result], func(s string) error {
+			if s == "" {
+				return fmt.Errorf("property value cannot be empty")
+			}
+			return nil
+		})
+		if err != nil {
+			return nil, err
+		}
+		stageProperties[result] = val
+	}
+	return stageProperties, nil
 }
