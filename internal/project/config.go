82be6206a5d327361f184277d98dfb5457207961
Leon Steinhäuser <42437185+leonsteinhaeuser@users.noreply.github.com>
2025-03-22 11:10:53 +0100

    chore: move logic related to config parsing and initialization to ParseConfig (#16)

--- internal/project/config.go
+++ internal/project/config.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/leonsteinhaeuser/openshift-gitops-cli/internal/template"
 	"sigs.k8s.io/yaml"
 )
 
@@ -20,6 +21,29 @@ func ParseConfig(path string) (*ProjectConfig, error) {
 		return nil, fmt.Errorf("failed to unmarshal config to ProjectConfig: %w", err)
 	}
 
+	if pc.Environments == nil {
+		pc.Environments = map[string]*Environment{}
+	}
+
+	if pc.Addons == nil {
+		pc.Addons = map[string]Addon{}
+	}
+
+	if pc.ParsedAddons == nil {
+		pc.ParsedAddons = map[string]template.TemplateManifest{}
+	}
+
+	// load all addons, so we can use them later
+	for k, v := range pc.Addons {
+		tm, err := template.LoadManifest(v.Path)
+		if err != nil {
+			return nil, fmt.Errorf("an error occurred while loading the addon [%s] manifest file: %s, %v", k, v.Path, err)
+		}
+		tm.Name = k
+		tm.BasePath = v.Path
+		tm.Group = v.Group
+		pc.ParsedAddons[k] = *tm
+	}
 	return pc, nil
 }
 
